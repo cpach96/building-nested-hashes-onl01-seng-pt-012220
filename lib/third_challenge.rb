9af398ed7ebb36c88dc730997730d43addb580a5
Curtis pachtinger <cpach96@gmail.com>
2020-02-03 02:20:29 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -8,6 +8,7 @@ def third_challenge
       :hero => {name: "Romeo", age: "15", status:"alive"},
       
       :hero_friends => []
+   }
    
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50" },
