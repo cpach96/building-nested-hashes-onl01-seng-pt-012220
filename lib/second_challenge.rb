147ae139f27cfab256cb98408d9db4e61d37c173
Curtis pachtinger <cpach96@gmail.com>
2020-02-03 02:09:11 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -4,13 +4,13 @@ def second_challenge
       :patriarch => {},
       :matriarch => {},
       :hero => {},
-      :hero_freinds => []
+      :hero_friends => []
       }, 
     :capulet => {
       :patriarch =>{},
       :matriarch => {},
       :heroine => {},
-      :heroine_reinds =>  []
+      :heroine_friends =>  []
       }
 
   }
